9d979e09d29d579a564dc8aa55aa2a22bba9a834
M V <vahyno+learn@gmail.com>
2017-10-10 06:03:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -99,13 +99,11 @@ function moveRock() {
      * we should remove the rock from the DOM
      */
 }
-
   // We should kick of the animation of the rock around here
-
+  window.requestAnimationFrame(moveRock)
   // Add the rock to ROCKS so that we can remove all rocks
   // when there's a collision
   ROCKS.push(rock)
-
   // Finally, return the rock element you've created
   return rock
 }
